14302bd2d6c34c1e02a54d6898fdccb88cb1a472
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-03-11 20:09:34 +1300

    Format JS config files as well.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // NOTE: this should stay last as it only disables rules that conflict with prettier
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier" // NOTE: this should stay last as it only disables rules that conflict with prettier
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "project": "./tsconfig.json",
-        "sourceType": "module",
+    ecmaVersion: 12,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "settings": {
-      "react": {
-        "version": "detect"
-      },
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/switch-exhaustiveness-check": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
